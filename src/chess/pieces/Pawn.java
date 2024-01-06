035b45d702078ac18bfb5648aab113f9a3de70e7
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-01-06 19:23:04 +0900

    add possible moves for pawn and for rook

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -16,6 +17,21 @@ public class Pawn extends ChessPiece {
     }
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[getBoard().getRows()][getBoard().getColumns()];
+        boolean[][] possibleMovesMatrix = new boolean[this.getBoard().getRows()][getBoard().getColumns()];
+        Position positionVerifier = new Position(0,0);
+
+        positionVerifier.setRow(this.position.getRow() - 1);
+        positionVerifier.setColumn(this.position.getColumn());
+
+        while(this.getBoard().positionExists(positionVerifier) && (!this.getBoard().thereIsAPiece(positionVerifier))){
+            possibleMovesMatrix[positionVerifier.getRow()][positionVerifier.getColumn()] = true;
+            positionVerifier.setRow(positionVerifier.getRow() - 1);
+        }
+
+        if(this.getBoard().positionExists(positionVerifier) && this.isThereOpponentPiece(positionVerifier)){
+            possibleMovesMatrix[positionVerifier.getRow()][positionVerifier.getColumn()] = true;
+        }
+
+        return possibleMovesMatrix;
     }
 }
--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -15,6 +16,45 @@ public class Rook extends ChessPiece {
     }
     @Override
     public boolean[][] possibleMoves() {
-        return new boolean[getBoard().getRows()][getBoard().getColumns()];
+        boolean[][] possibleMovesMatrix = new boolean[this.getBoard().getRows()][getBoard().getColumns()];
+        Position positionVerifier = new Position(0,0);
+
+        //above
+        positionVerifier.setRow(this.position.getRow() - 1);
+        positionVerifier.setColumn(this.position.getColumn());
+
+        while(this.getBoard().positionExists(positionVerifier) && (!this.getBoard().thereIsAPiece(positionVerifier))){
+            possibleMovesMatrix[positionVerifier.getRow()][positionVerifier.getColumn()] = true;
+            positionVerifier.setRow(positionVerifier.getRow() - 1);
+        }
+
+        if(this.getBoard().positionExists(positionVerifier) && this.isThereOpponentPiece(positionVerifier)){
+            possibleMovesMatrix[positionVerifier.getRow()][positionVerifier.getColumn()] = true;
+        }
+
+        //left
+        positionVerifier.setRow(this.position.getRow());
+        positionVerifier.setColumn(this.position.getColumn() - 1);
+
+        while(this.getBoard().positionExists(positionVerifier) && (!this.getBoard().thereIsAPiece(positionVerifier))){
+            possibleMovesMatrix[positionVerifier.getRow()][positionVerifier.getColumn()] = true;
+            positionVerifier.setRow(positionVerifier.getColumn() - 1);
+        }
+        if(this.getBoard().thereIsAPiece(positionVerifier) && this.isThereOpponentPiece(positionVerifier)){
+            possibleMovesMatrix[positionVerifier.getRow()][positionVerifier.getColumn()] = true;
+        }
+
+        //right
+        positionVerifier.setRow(this.position.getRow());
+        positionVerifier.setColumn(this.position.getColumn() + 1);
+
+        while(this.getBoard().positionExists(positionVerifier) && this.getBoard().thereIsAPiece(positionVerifier)){
+            possibleMovesMatrix[positionVerifier.getRow()][positionVerifier.getColumn()] = true;
+            positionVerifier.setColumn(positionVerifier.getColumn() + 1);
+        }
+        if(this.getBoard().thereIsAPiece(positionVerifier) && this.isThereOpponentPiece(positionVerifier)){
+            possibleMovesMatrix[this.getBoard().getRows()][this.getBoard().getColumns()] = true;
+        }
+        return possibleMovesMatrix;
     }
 }
