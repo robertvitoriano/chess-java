a82a13867ee48428e8e7bdb04272315d46e36226
Robert Vitoriano <robertvitorianodev@gmail.com>
2024-01-07 23:54:52 +0900

    finish logic for pawn movements

--- src/chess/pieces/Pawn.java
+++ src/chess/pieces/Pawn.java
@@ -12,6 +12,7 @@ public class Pawn extends ChessPiece {
     }
 
     private int pawnMoves = 0;
+    private int direction = this.getColor() == Color.WHITE ? -1 : 1;
 
     @Override
     public String toString() {
@@ -27,19 +28,18 @@ public class Pawn extends ChessPiece {
         boolean[][] possibleMovesMatrix = new boolean[this.getBoard().getRows()][getBoard().getColumns()];
         Position positionVerifier = new Position(0, 0);
 
-        positionVerifier.setRow(this.position.getRow() - 1);
+        positionVerifier.setRow(this.position.getRow() + this.direction);
         positionVerifier.setColumn(this.position.getColumn());
-        Boolean canMove = !this.getBoard().thereIsAPiece(positionVerifier) || this.isThereOpponentPiece(positionVerifier);
-                if (this.pawnMoves > 1) {
+        Boolean canMove = !this.getBoard().thereIsAPiece(positionVerifier);
+        if (this.pawnMoves > 1) {
             if (this.getBoard().positionExists(positionVerifier) && canMove) {
                 possibleMovesMatrix[positionVerifier.getRow()][positionVerifier.getColumn()] = true;
             }
         } else if (this.getBoard().positionExists(positionVerifier) && canMove && this.pawnMoves == 1 ) {
             possibleMovesMatrix[positionVerifier.getRow()][positionVerifier.getColumn()] = true;
-            positionVerifier.setRow(positionVerifier.getRow() - 1);
-            canMove = !this.getBoard().thereIsAPiece(positionVerifier) || this.isThereOpponentPiece(positionVerifier);
+            positionVerifier.setRow(positionVerifier.getRow() + this.direction);
+            canMove = !this.getBoard().thereIsAPiece(positionVerifier);
     
-            
             if (this.getBoard().positionExists(positionVerifier) && canMove) {
                 possibleMovesMatrix[positionVerifier.getRow()][positionVerifier.getColumn()] = true;
             }
